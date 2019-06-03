4403ed43b01be614234fc3db774f661ce2be051c
Camila Herculano <camilaherculano@users.noreply.github.com>
2019-06-03 16:45:37 -0400

    add stuff

---
+++ src/store/actions.js
@@ -0,0 +1,30 @@
+export const GET_UNSPLASH = 'GET_UNSPLASH';
+export const SUCCESS_UNSPLASH = 'SUCCESS_UNSPLASH';
+export const ERROR_UNSPLASH = 'ERROR_UNSPLASH';
+
+export const getUnsplash = term => {
+  return {
+    type: GET_UNSPLASH,
+    payload: {
+      term: term
+    }
+  };
+};
+
+export const successUnsplash = images => {
+  return {
+    type: SUCCESS_UNSPLASH,
+    payload: {
+      images: images
+    }
+  };
+};
+
+export const errorUnsplash = error => {
+  return {
+    type: ERROR_UNSPLASH,
+    payload: {
+      error: error
+    }
+  };
+};
